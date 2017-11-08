4cf0b180c759f9c94a2299113acf0c6433f29197
Matt English <menglish2451@gmail.com>
2017-11-07 20:35:00 -0800

    Add player and imp sprite placeholder

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+    game.load.image('imp', 'assets/sprites/imp.png');
+    game.load.image('player', 'assets/sprites/player.png');
 }
 
 function create() {
+    game.add.sprite(100, 0, 'player');
+    game.add.sprite(0, 0, 'imp');
 }
 
 function update() {
