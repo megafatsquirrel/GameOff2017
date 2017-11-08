a111991be37a8c3491f13429a81039ae002ac3fe
Matt English <menglish2451@gmail.com>
2017-11-07 22:23:47 -0800

    add grassfield

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-var game = new Phaser.Game(1280, 720, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
     game.load.image('imp', 'assets/sprites/imp.png');
     game.load.image('player', 'assets/sprites/player.png');
+    game.load.image('grassField', 'assets/sprites/grassField.png');
 }
 
 function create() {
+    game.add.sprite(0, 0, 'grassField');
     game.add.sprite(100, 0, 'player');
     game.add.sprite(0, 0, 'imp');
+    
 }
 
 function update() {
