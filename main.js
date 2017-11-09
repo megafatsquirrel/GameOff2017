0d15478d84b6463a472e76883f362c4a49124533
Matt English <menglish2451@gmail.com>
2017-11-08 17:51:13 -0800

    Add sword attack logic

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var game = new Phaser.Game(800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var player;
+var sword;
 var speed = 4;
 var worldBounds;
 
@@ -19,10 +20,19 @@ function create() {
     worldBounds.anchor.set(0.5);
 
     player = game.add.sprite(game.world.centerX, game.world.centerY, 'player');
+    player.facing = {
+        top: false,
+        left: false,
+        right: false,
+        down: true // default to this?
+    };
     player.anchor.setTo(0.5, 0.5);
     player.inputEnabled = true;
     player.input.boundsSprite = worldBounds;
     game.camera.follow(player);
+
+    sword = game.add.sprite(player.position.x, player.position.y, 'sword');
+    sword.visible = false;
 
     game.add.sprite(game.world.centerX - 100, game.world.centerY, 'imp');
 }
@@ -30,19 +40,78 @@ function create() {
 function update() {
     if (game.input.keyboard.isDown(Phaser.Keyboard.A))
     {
+        setPlayerFacing('left');
+        sword.currentFacing = 'left';
         player.x -= speed;        
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.D))
     {
+        setPlayerFacing('right');
+        sword.currentFacing = 'right';
         player.x += speed;
     }
 
     if (game.input.keyboard.isDown(Phaser.Keyboard.W))
     {
+        setPlayerFacing('top');
+        sword.currentFacing = 'top';
         player.y -= speed;        
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.S))
     {
+        setPlayerFacing('down');
+        sword.currentFacing = 'down';
         player.y += speed;
     }
+
+    // Attack
+    if (game.input.keyboard.isDown(Phaser.Keyboard.E)) {
+        sword.position.x = player.position.x;
+        sword.position.y = player.position.y;
+        sword.visible = true;
+        game.time.events.add(250, removeSword, this, true);
+    }
+
+    if (sword.visible){
+        sword.position.x = player.position.x;
+        sword.position.y = player.position.y;
+        adjustSwordPosition();
+    }
+}
+
+function setPlayerFacing(face){
+    if (face && typeof String){
+        player.facing.top = face === 'top' ? true : false;
+        player.facing.left = face === 'left' ? true : false;
+        player.facing.right = face === 'right' ? true : false;
+        player.facing.down = face === 'down' ? true : false;
+    }else{
+        console.warn('Please use a string, "top", "left", "right", or "down"');
+    }
+
+}
+
+function adjustSwordPosition(){
+    if(player.facing.top){
+        sword.angle = 0;
+        sword.position.y -= 60; // Add the height of the player
+    }
+    if(player.facing.left){
+        sword.angle = 90;
+        sword.position.y -= 10;
+        sword.position.x -= 20; // Remove the width of the player
+    }
+    if(player.facing.right){
+        sword.angle = -90;
+        sword.position.y += 10;
+        sword.position.x += 20; // Remove the width of the player
+    }
+    if(player.facing.down){
+        sword.angle = 0;
+        sword.position.y += 20;
+    }
+}
+
+function removeSword(){
+    sword.visible = false;
 }
