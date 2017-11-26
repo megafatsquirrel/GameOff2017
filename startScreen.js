45a9900c64c304bdb2993aa9139d0044b8a3f025
Matt English <menglish2451@gmail.com>
2017-11-25 22:50:20 -0800

    Add some text to the start screen

--- startScreen.js
+++ startScreen.js
@@ -4,6 +4,9 @@ startScreen.prototype = {
 		var image = game.add.sprite(0, 0, 'startScreen');
 		image.inputEnabled = true;
 		image.events.onInputDown.add(this.listener, this);
+
+		var style = { font: "14px Arial", fill: "#ff0044", align: "center" };
+		game.add.text(32, 30, 'Click to start', style);
 	},
 	listener: function(){
 		this.game.state.start("mainGame");
