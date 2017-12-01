1d2795c901c0539ce84c461e29b897d2b2216500
Matt English <menglish2451@gmail.com>
2017-12-01 11:06:12 -0800

    Add control instructions

--- startScreen.js
+++ startScreen.js
@@ -9,6 +9,12 @@ startScreen.prototype = {
 		game.add.text(330, 100, 'Gedsted Arena', style);
 		style = { font: "48px Arial", fill: "#000000", align: "center" };
 		game.add.text(410, 500, 'Click to start', style);
+		style = { font: "24px Arial", fill: "#ffffff", align: "left" };
+		game.add.text(30, 250, 'CONTROLS', style);
+		game.add.text(30, 275, 'W A S D: move', style);
+		game.add.text(30, 300, 'E: attack', style);
+		game.add.text(30, 325, 'Q: block', style);
+		game.add.text(30, 350, 'SHIFT: sprint', style);
 	},
 	listener: function(){
 		this.game.state.start("mainGame");
